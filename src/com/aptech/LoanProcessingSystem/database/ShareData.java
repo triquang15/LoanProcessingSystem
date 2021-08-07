7f710bd0267a354f3ff6c6fd8d72e50515f43dde
triquang15 <triquang.15qt@gmail.com>
2021-08-07 21:07:24 +0700

    Share Data

---
+++ src/com/aptech/LoanProcessingSystem/database/ShareData.java
@@ -0,0 +1,8 @@
+package com.aptech.LoanProcessingSystem.database;
+
+import com.aptech.LoanProcessingSystem.entities.Account;
+
+public class ShareData {
+	public static Account accountLogin = null;
+
+}
